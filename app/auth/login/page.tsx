1e5b0ad0b1c2dd62cfe5bcace13452656ebc69c5
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-01-26 01:53:28 -0400

    add login page component

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Button from '@elements/Button'
+import TextBox from '@elements/TextBox'
+import { useRef } from 'react'
+
+const LoginPage = () => {
+  const userName = useRef('')
+  const pass = useRef('')
+
+  const onSubmit = async () => {}
+
+  return (
+    <div
+      className={
+        'flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600'
+      }
+    >
+      <div className='px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2'>
+        <TextBox
+          lableText='User Name'
+          onChange={(e) => (userName.current = e.target.value)}
+        />
+        <TextBox
+          lableText='Password'
+          type={'password'}
+          onChange={(e) => (pass.current = e.target.value)}
+        />
+        <Button onClick={onSubmit}>Login</Button>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
