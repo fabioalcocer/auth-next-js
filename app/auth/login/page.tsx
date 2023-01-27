93a501d9b140b78c109d04c7fb5397e468efd380
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-01-27 00:42:52 -0400

    add onSubmit function and search params prop

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,25 @@
 'use client'
 import Button from '@elements/Button'
 import TextBox from '@elements/TextBox'
+import { signIn } from 'next-auth/react'
 import { useRef } from 'react'
 
-const LoginPage = () => {
+interface IProps {
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+const LoginPage = ({ searchParams}: IProps) => {
   const userName = useRef('')
   const pass = useRef('')
 
-  const onSubmit = async () => {}
+  const onSubmit = async () => {
+    const result = await signIn('credentials', {
+      username: userName.current,
+      password: pass.current,
+      redirect: true,
+      callbackUrl: '/',
+    })
+  }
 
   return (
     <div
@@ -15,6 +27,11 @@ const LoginPage = () => {
         'flex flex-col justify-center items-center  h-screen bg-gradient-to-br gap-1 from-cyan-300 to-sky-600'
       }
     >
+      {searchParams?.message && (
+        <p className='text-red-700 bg-red-100 py-2 px-5 rounded-md'>
+          {searchParams?.message}
+        </p>
+      )}
       <div className='px-7 py-4 shadow bg-white rounded-md flex flex-col gap-2'>
         <TextBox
           lableText='User Name'
